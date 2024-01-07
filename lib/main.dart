2d418dc1dec99598f5c4cdef12567a3cd2cead12
moorthikin <lmmoorthi0@gmail.com>
2024-01-07 22:07:08 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firstapp/pages/login_page.dart';
 import 'package:firstapp/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 //It was the entry point of the flutter application
 void main() {
@@ -14,13 +15,17 @@ class FirstApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.light,
       darkTheme:
           ThemeData(brightness: Brightness.dark, primarySwatch: Colors.red),
       theme: ThemeData(
+        //primaryTextTheme: GoogleFonts.latoTextTheme(),
+        fontFamily: GoogleFonts.lato().fontFamily,
         primarySwatch: Colors.deepPurple,
       ),
-      initialRoute: "/home",
+      //It was the initial Route when application start the this screen will first appear.
+      initialRoute: "/",
+      //we declare a routes "/" it was the default route after we customize as per our wish.
       routes: {
         "/": (context) => const LoginPage(),
         "/home": (context) => const HomePage()
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,16 +6,59 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lime,
-      appBar: AppBar(
-        backgroundColor: Colors.pinkAccent,
-        title: const Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
-        
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Image.asset(
+                "assets/images/login.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "User Name", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: "Pass Word", labelText: "Password"),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Hello Flutter");
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll(Colors.indigo)),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
-        centerTitle: true,
       ),
     );
   }
