43e4817e6e48fd7cd96158ac87543f4d7f799fcf
moorthikin <lmmoorthi0@gmail.com>
2024-01-07 18:15:55 +0530

    first app related changes

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FirstApp());
+}
+
+class FirstApp extends StatelessWidget {
+  const FirstApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            child: Text("First App"),
+          ),
+        ),
+      ),
+    );
+  }
+}
