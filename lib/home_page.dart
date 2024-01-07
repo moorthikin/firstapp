9e616e095d692e944cfdc7e0cc94ea370bf2ef8a
moorthikin <lmmoorthi0@gmail.com>
2024-01-07 18:41:57 +0530

    day1 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int day1 = 30;
+  String name = "Moorthi!";
+  double pi = 3.14;
+  bool isMale = true;
+  num temp = 30.8;
+
+  var day = 76;
+  final pii = 3.14;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First Application  "),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: Container(
+          child: Text("First $day1 $name App " + name),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
